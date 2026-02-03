4c77ad4d51c546d97fa537aa54e4c5968e1b0246
wangmu <wangmu8889@gmail.com>
2026-02-03 21:50:31 +0800

    merge

---
+++ fluent_python/iter_gene/using_zip.py
@@ -0,0 +1,11 @@
+"""Iterating Over Multiple Sequences Simultaneously
+
+zip(*iterables, strict=False)
+"""
+
+if __name__ == "__main__":
+    xpts = [1, 5, 4, 2, 10, 7]
+    ypts = [101, 78, 37, 15, 62, 99]
+    for x, y in zip(xpts, ypts):
+        print(f"({x}, {y})", end=",    ")
+    print("\n" + "=" * 80)
